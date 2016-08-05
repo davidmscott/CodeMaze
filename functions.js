5c219ff59924a10f6b41520b81fcc88ff8196639
davidmscott <davescott@Daves-MacBook-Pro.local>
2016-08-05 15:33:23 -0600

    Added to function.js

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 var currentpos = [xpos,ypos];
 var direction = [0, 0];
+var questionNum = 0;
+// set guestionList variable
 
 function move(){
 	while (mazearray[xpos + direction[0]][ypos + direction[1]]) {
@@ -20,7 +22,52 @@ function move(){
 }
 
 function callQ() {
-	
+	questionNum++;
+	$("#answer").val("");
+	// need to create :hover and assign to possible answers to arrows
+	if (questionList[questionNum].up !== "") {
+		$('#col' + currentpos[0] + 'row' + (currentpos[1] - 1)).css({
+			'background-image': '/uparrow.img/'
+		});
+	}
+		if (questionList[questionNum].right !== "") {
+		$('#col' + (currentpos[0] + 1)+ 'row' + currentpos[1]).css({
+			'background-image': '/rightarrow.img/'
+		});
+	}
+		if (questionList[questionNum].down !== "") {
+		$('#col' + currentpos[0] + 'row' + (currentpos[1] + 1)).css({
+			'background-image': '/downarrow.img/'
+		});
+	}
+		if (questionList[questionNum].left !== "") {
+		$('#col' + (currentpos[0] - 1) + 'row' + currentpos[1]).css({
+			'background-image': '/leftarrow.img/'
+		});
+	}
+}
+
+function submitQ() {
+	var answer = $("#answer").val();
+	if (answer === "") {
+		alert("You must type your answer in the text box.");
+	} else if (answer === questionList[questionNum].up) {
+		direction = [0, -1];
+		move();
+		return;
+	} else if (answer === questionList[questionNum].right) {
+		direction = [1, 0];
+		move();
+		return;
+	} else if (answer === questionList[questionNum].down) {
+		direction = [0, 1];
+		move();
+		return;
+	} else if (answer === questionList[questionNum].left) {
+		direction = [-1, 0];
+		move();
+		return;
+	}
 }
 
 function displayPlayer() {
@@ -37,9 +84,13 @@ function displayPlayer() {
 }
 
 function youWin() {
+	// temporary
+	alert("You win");
 	// write function for what happens when player completes the maze
 }
 
 function youLose() {
+	// temporary
+	alert("You lose");
 	// write function for what happens when player loses/goes off the board...return to last question?
 }
